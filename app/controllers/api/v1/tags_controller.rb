628e3215278cbe36a666db3e266314362b2d9dd3
榑林晃平 <kure.kohei.321@docomo.ne.jp>
2024-03-06 09:21:17 +0900

    [update] リファクタ

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class TagsController < ApplicationController
       def index
-        tags = Tag.eager_load(:posts)
+        tags = Tag.eager_load(:posts).pluck(:id, :name)
         render json: tags
       end
     end
