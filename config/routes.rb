9d24b220a109284289243f7037fed163d132a28b
榑林晃平 <kouhei.kurebayashi@goals.co.jp>
2024-06-05 03:12:17 +0900

    [update] カテゴリーに紐づく投稿アクションのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace 'api' do
     namespace 'v1' do
+      get 'posts/search', to: 'posts#search'
       resources :posts, only: [:index, :create, :show]
       patch 'posts/:id/update', to: 'posts#update'
       delete 'posts/:id/delete', to: 'posts#delete'
 
       resources :tags
       resources :categories
+      get 'categories/:id/posts', to: 'categories#posts'
 
-      get 'posts/search', to: 'posts#search'
     end
   end
 end
