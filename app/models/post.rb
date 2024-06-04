1f26a83e8f3f4ccafa025f8b7697d04afe33bf69
榑林晃平 <kouhei.kurebayashi@goals.co.jp>
2024-06-05 03:12:51 +0900

    [refactor] リファクタ

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,9 @@ class Post < ApplicationRecord
   has_many :tags, through: :post_tags
   has_many :post_categories, dependent: :destroy
   has_many :categories, through: :post_categories
-  scope :is_not_deleted, -> {
-    where(is_deleted: false)
-  }
+
+
+  scope :is_not_deleted, -> { where(is_deleted: false) }
 
   scope :search_by_keyword, ->(search_word) {
     where(["title LIKE(?) OR content LIKE(?)", "%#{search_word}%", "%#{search_word}%"])
